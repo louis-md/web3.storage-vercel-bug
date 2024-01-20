05eb6e573e7c3adbd5556851c20152a6c539ff74
louis-md <louis@margot-duclot.fr>
2024-01-20 16:42:31 +0100

    Update tsconfig & next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  images: {
+    domains: ['directus.messari.io']
+  },
+  staticPageGenerationTimeout: 180,
+  generateBuildId: () => 'build',
+  webpack: config => {
+    config.resolve.fallback = {
+      net: false,
+      tls: false,
+      fs: false,
+      path: false,
+      assert: ['assert'],
+      stream: ['stream-browserify'],
+      crypto: ['crypto-browserify'],
+      http: ['stream-http'],
+      https: ['https-browserify'],
+      os: ['os-browserify']
+    }
+    return config
+  }
+}
 
-export default nextConfig;
+export default nextConfig
